61bbb65d55cfbe4a56edd68a0bd47c368b0e14f3
diegocalzadilla09 <diegocalzadilla09@gmail.com>
2025-11-10 18:44:42 +0000

    generador de excusas 2.0

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
+//antes= window.onload = function ()
+window.onload = function generator () {
     let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
     let action = ['ate', 'peed', 'crushed', 'broke'];
     let what = ['my homework', 'my phone', 'the car'];
